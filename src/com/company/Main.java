d4424d4155c8334c5e202c5a5b4b1cedc0b43df3
Murphy Hsieh <murphyhsieh@leandev.io>
2020-05-18 10:19:17 +0800

    turn the location class into immutable class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,29 +9,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> tempExits = new HashMap<String, Integer>();
 
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExits));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
-        locations.get(1).addExit("Q", 0);
+        tempExits = new HashMap<String, Integer>();
+        tempExits.put("W", 2);
+        tempExits.put("E", 3);
+        tempExits.put("S", 4);
+        tempExits.put("N", 5);
+        tempExits.put("Q", 0);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExits));
 
-        locations.get(2).addExit("N", 5);
+        tempExits = new HashMap<String, Integer>();
+        tempExits.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExits));
 
-        locations.get(3).addExit("W", 1);
+        tempExits = new HashMap<String, Integer>();
+        tempExits.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExits));
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
+        tempExits = new HashMap<String, Integer>();
+        tempExits.put("N", 1);
+        tempExits.put("W", 2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExits));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+        tempExits = new HashMap<String, Integer>();
+        tempExits.put("S", 1);
+        tempExits.put("W", 2);
+        locations.put(5, new Location(5, "You are in the forest", tempExits));
 
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT", "Q");
@@ -43,6 +49,7 @@ public class Main {
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
+            tempExits.remove("S"); /** 如果 constructor 不重新 new HashMap(等同於 reference tempExits) 會被影響 **/
             if (loc == 0) {
                 break;
             }
